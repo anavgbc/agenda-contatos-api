c6011acd14af8162f97951c891e37e4608f4bdf8
anavgbc <agomesbc@gmail.com>
2023-03-27 14:19:28 -0300

    fix: entities path

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,4 +1,8 @@
 import { DataSource, DataSourceOptions } from "typeorm";
+import { User } from "./entities/user.entity";
+import { Contacts } from "./entities/contact.entity";
+import { initialMigration1679922949927 } from "./migrations/1679922949927-initialMigration";
+import { createTables1679922962557 } from "./migrations/1679922962557-createTables";
 import "dotenv/config";
 
 const setDataSourceConfig = (): DataSourceOptions => {
@@ -8,8 +12,8 @@ const setDataSourceConfig = (): DataSourceOptions => {
     return {
       type: "postgres",
       url: process.env.DATABASE_URL,
-      entities: ["src/entities/*.ts"],
-      migrations: ["src/migrations/*.ts"],
+      entities: [User, Contacts],
+      migrations: [initialMigration1679922949927, createTables1679922962557],
     };
   }
   return {
